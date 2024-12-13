4f16fff423b9b05b9b319d390122760822c5f8fd
summer21000 <wkrehreo1999@gmail.com>
2024-12-13 15:17:19 +0900

    2024-12-13 spring jpa News 수정

--- spring/jpa/jpa/src/main/java/com/example/jpa/domain/News.java
+++ spring/jpa/jpa/src/main/java/com/example/jpa/domain/News.java
@@ -1,13 +1,25 @@
 package com.example.jpa.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class News {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long newsId;
+
+    @Column(name = "news_title", nullable = false, length = 254)
     private String title;
+
+    @Column(nullable = false)
     private String content;
 }
