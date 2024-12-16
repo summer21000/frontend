4f2e16a63d342c5147a796bee595fbd201068e2d
summer21000 <wkrehreo1999@gmail.com>
2024-12-16 17:19:35 +0900

    2024-12-16 spring 프로젝트 News Appconfig 오타 수정

--- spring/05. news/News/src/main/java/com/example/News/config/AppConfig.java
+++ spring/05. news/News/src/main/java/com/example/News/config/AppConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addRedirectViewController("/", "/new/list");
+        registry.addRedirectViewController("/", "/news/list");
     }
 }
