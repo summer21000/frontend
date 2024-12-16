44b3b5c5c16b10b4b54b807587f5cc6053037685
summer21000 <wkrehreo1999@gmail.com>
2024-12-16 16:43:55 +0900

    2024-12-16 spring 프로젝트 News 페이지간 리다이렉션 및 링크이동 커밋

--- spring/05. news/News/src/main/java/com/example/News/controller/NewsController.java
+++ spring/05. news/News/src/main/java/com/example/News/controller/NewsController.java
@@ -8,31 +8,49 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/news")
 public class NewsController {
     private final NewsRepository newsRepository;
 
     private final NewsMapper mapper;
 
-    @GetMapping("/news/new")
+    @GetMapping("/new")
     public String newArticleForm(){
         return "news/new";
     }
-    @PostMapping("/news/create")
+    @PostMapping("/create")
     public String createNews(NewsDto.Post post){
-        System.out.println(post.toString());
-        News n = mapper.newPostDtoToNews(post);
-        System.out.println(n);
-        newsRepository.save(n);
-        return "";
+        News news = mapper.newPostDtoToNews(post);
+        newsRepository.save(news);
+        return "redirect:/news/" + news.getNewsId();
     }
 
+    @GetMapping("/{newsId}")
+    public String getNews(@PathVariable("newsId") Long newsId, Model model) {
+        News news = newsRepository.findById(newsId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 계시글이 없습니다."));
+        model.addAttribute("news", news);
+        return "news/detail";
+    }
+
+    @GetMapping("/list")
+    public String getNewsList(Model model){
+        List<News> newsList = newsRepository.findAll();
+        model.addAttribute("newsList", newsList);
+        return "news/list";
+    }
     //엔티티를 그대로 사용하는 예시
-    @PostMapping("/news/create2")
+    @PostMapping("/create2")
     public String createNews(News news){
         newsRepository.save(news);
         return "";
