91a5d610905ae54f7af1590ae5dd1da3f3a7d26e
summer21000 <wkrehreo1999@gmail.com>
2024-12-17 14:52:11 +0900

    2024-12-16 spring 프로젝트 book vaildation 의존성 추가 DTO에 vaildation 요소 추가

--- spring/06. book/book/src/main/java/com/example/book/dto/BookDTO.java
+++ spring/06. book/book/src/main/java/com/example/book/dto/BookDTO.java
@@ -1,6 +1,9 @@
 package com.example.book.dto;
 
 import com.example.book.domain.Book;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,20 @@ public class BookDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Post{
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
+        @NotNull(message = "제목은 반드시 입력하셔야 합니다.")
+        @Size(min=1, max = 45, message = "제목은 45자 이하여야 합니다.")
         private String title;
+
+        @NotBlank
+        @NotNull
+        @Size(max = 45)
         private String subTitle;
+
+        @Size(max = 45)
         private String author;
+
+        @Size(max = 45)
         private String publisher;
     }
 
@@ -23,10 +37,23 @@ public class BookDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Put{
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
+        @NotNull(message = "제목은 반드시 입력하셔야 합니다.")
+        @Size(min=1, max = 45, message = "제목은 45자 이하여야 합니다.")
         private String title;
+
+        @NotBlank
+        @NotNull
+        @Size(max = 45)
         private String subTitle;
+
+        @Size(max = 45)
         private String author;
+
+        @Size(max = 45)
         private String publisher;
+
+        @Size(max = 45)
         private Book.Status status;
     }
 
@@ -36,6 +63,27 @@ public class BookDTO {
     @AllArgsConstructor
     public static class Patch{
         private Book.Status status;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Rollback{
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
+        @NotNull(message = "제목은 반드시 입력하셔야 합니다.")
+        @Size(min=1, max = 45, message = "제목은 45자 이하여야 합니다.")
+        private String title;
+
+        @NotBlank
+        @NotNull
+        @Size(max = 45)
+        private String subTitle;
+
+        @NotBlank
+        @NotNull
+        @Size(max = 45)
+        private String publisher;
     }
 
     @Getter
